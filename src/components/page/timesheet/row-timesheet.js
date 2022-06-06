4714ebbbb995528f639987e3a40b0ed0b7a8d9e3
Do Chi Cong <congdc@vnext.vn>
2022-06-06 14:13:03 +0700

    table

---
+++ src/components/page/timesheet/row-timesheet.js
@@ -0,0 +1,84 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react'
+import moment from 'moment'
+import ForgetModal from './forgetModal/forgetModal'
+import LeaveModal from './leaveModal/leaveModal'
+
+const RowTimesheet = ({ row }) => {
+  const [isOpen, setIsOpen] = useState({
+    isOpenForget: false,
+    isOpenLeave: false,
+  })
+  const handleClickModal = (type) => {
+    const modalType = type.toUpperCase()
+    switch (modalType) {
+      case 'FORGET':
+        setIsOpen({
+          ...isOpen,
+          isOpenForget: !isOpen.isOpenForget,
+        })
+        break
+      case 'LEAVE':
+        setIsOpen({
+          ...isOpen,
+          isOpenLeave: !isOpen.isOpenLeave,
+        })
+        break
+      default:
+        throw new Error('dm co loi roi')
+    }
+  }
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        margin: '20px',
+      }}
+    >
+      <div>{moment.unix(row.work_date).format('YYYY/MM/DD')}</div>
+      <div>{moment(row.check_in).format('hh:mm:ss a')}</div>
+      <div>{moment(row.check_out).format('hh:mm:ss a')}</div>
+      <div>{row.late}</div>
+      <div>{row.early}</div>
+      <div>{row.work_time}</div>
+      <div>{row.in_office}</div>
+      <button
+        type="button"
+        onClick={() => {
+          handleClickModal('forget')
+        }}
+      >
+        Forget
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          handleClickModal('leave')
+        }}
+      >
+        Leave
+      </button>
+      {isOpen.isOpenForget && (
+        <ForgetModal
+          isOpen={isOpen.isOpenForget}
+          row={row}
+          handleCloseForget={() => {
+            setIsOpen((isOpen.isOpenForget = false))
+          }}
+        ></ForgetModal>
+      )}
+      {isOpen.isOpenLeave && (
+        <LeaveModal
+          isOpen={isOpen.isOpenLeave}
+          row={row}
+          handleCloseLeave={() => {
+            setIsOpen((isOpen.isOpenLeave = false))
+          }}
+        ></LeaveModal>
+      )}
+    </div>
+  )
+}
+
+export default RowTimesheet
