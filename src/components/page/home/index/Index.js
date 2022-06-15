9eebebd470338eda00844a1baded8813d136179e
Pham Van Liem <liempv@vnext.vn>
2022-06-15 08:58:58 +0700

    list NOtice

--- src/components/page/home/index/Index.js
+++ src/components/page/home/index/Index.js
@@ -1,6 +1,6 @@
 /* disable eslint object-curly-spacing */
-import React, { useEffect } from 'react'
-import { Row, Col } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Row, Col, Modal } from 'antd'
 import styles from './Index.module.scss'
 import './Index.scss'
 import { dateTime, CMTable } from '../../../index'
@@ -8,15 +8,14 @@ import { getDataListNotice } from '../slice/slice'
 import { useDispatch, useSelector } from 'react-redux'
 
 const Index = () => {
-  const dispatch = useDispatch()
-  const dataTable = useSelector((state) => {
-    return state.noticeList.tableData
-  })
-
+  const [modal, setModal] = useState({ open: false, data: {} })
   const stateNotice = useSelector((state) => {
     return state.noticeList
   })
 
+  console.log('modal', modal)
+
+  const dispatch = useDispatch()
   useEffect(() => {
     dispatch(getDataListNotice({ perPage: 10, page: 1 }))
   }, [])
@@ -95,22 +94,20 @@ const Index = () => {
       title: <p className={styles.whiteColor}>Detail</p>,
       dataIndex: 'detail',
       key: 'detail',
-      render: (payload) => {
+      render: (payload, record) => {
         return (
-          <a
-            href="https://github.com/thanhliem26/SearchUserGitHub/blob/main/src/components/pages/UserSearch/UserList/UserInfo.js"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p
+            className="tb_center"
+            onClick={() => setModal({ open: true, data: record })}
           >
             View
-          </a>
+          </p>
         )
       },
     },
   ]
 
   const onShowSizeChange = (page, size) => {
-    console.log('check', page, size)
     dispatch(getDataListNotice({ perPage: size, page: page }))
   }
 
@@ -198,7 +195,7 @@ const Index = () => {
             }}
             // pagination={{ pageSize: page }}
             className="tableNotice"
-            data={dataTable}
+            data={stateNotice.tableData}
             // remove={['published_to']}
             columns={columns}
             sorter={{ no: 'number', author: 'string', publishedDate: 'date' }}
@@ -213,7 +210,7 @@ const Index = () => {
               published_date: { position: 'tb_center' },
               detail: { position: 'tb_center' },
             }}
-            styleBody={{ detail: { position: 'tb_end' } }}
+            // styleBody={{ detail: { position: 'tb_center' } }}
             pagination={{
               current: stateNotice.currentPage,
               total: stateNotice.total,
@@ -224,6 +221,105 @@ const Index = () => {
           />
         </Col>
       </Row>
+      <Modal
+        wrapClassName="modalNotice"
+        title="Notice Detail"
+        visible={modal.open}
+        onCancel={() =>
+          setModal((prev) => {
+            return { ...prev, open: false }
+          })
+        }
+      >
+        <Row>
+          <Col
+            xs={24}
+            md={24}
+            xl={24}
+            style={{ display: 'flex', borderBottom: '2px solid #ab9f9f' }}
+          >
+            <Col xs={12} md={12} xl={12}>
+              <h3 style={{ marginBottom: '20px' }}>User Member</h3>
+              <Col xs={24} md={24} xl={24}>
+                <div className="formGroup">
+                  <i className="fa-solid fa-user"></i>
+                  <div className="formGroupText">
+                    <p>Name:</p>
+                    <p>Berenice Connelly</p>
+                  </div>
+                </div>
+              </Col>
+              <Col xs={24} md={24} xl={24}>
+                <div className="formGroup">
+                  <i className="fa-solid fa-envelope"></i>
+                  <div className="formGroupText">
+                    <p>Email:</p>
+                    <p>vinaphone232@gmail.com</p>
+                  </div>
+                </div>
+              </Col>
+              <Col xs={24} md={24} xl={24}>
+                <div className="formGroup">
+                  <i className="fa-solid fa-user"></i>
+                  <div className="formGroupText">
+                    <p>Nick name:</p>
+                    <p>NamDx Update composer</p>
+                  </div>
+                </div>
+              </Col>
+              <Col xs={24} md={24} xl={24}>
+                <div className="formGroup">
+                  <i className="fa-solid fa-phone"></i>
+                  <div className="formGroupText">
+                    <p>Phone:</p>
+                    <p>0263862877</p>
+                  </div>
+                </div>
+              </Col>
+            </Col>
+            <Col xs={12} md={12} xl={12}>
+              <h3 style={{ marginBottom: '20px' }}>To Department</h3>
+              <Col xs={24} md={24} xl={24}>
+                <div className="formGroup">
+                  <i className="fa-solid fa-building"></i>
+                  <div className="formGroupText">
+                    <p>To department:</p>
+                    <p>
+                      {modal.data.published_to &&
+                        modal.data.published_to[0].division_name}
+                    </p>
+                  </div>
+                </div>
+              </Col>
+              <Col xs={24} md={24} xl={24}>
+                <div className="formGroup">
+                  <i className="fa-solid fa-calendar"></i>
+                  <div className="formGroupText">
+                    <p>Published date: </p>
+                    <p>{modal?.data?.published_date}</p>
+                  </div>
+                </div>
+              </Col>
+            </Col>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={24} md={24} xl={24}>
+            <h3 style={{ marginBottom: '20px' }}>Detail</h3>
+          </Col>
+          <Col xs={24} md={24} xl={24}>
+            <p style={{ fontWeight: 600 }}>Subject: {modal?.data?.subject}</p>
+          </Col>
+          <Col xs={24} md={24} xl={24}>
+            <p style={{ fontWeight: 600 }}>
+              Attachment: {modal?.data?.attachment}
+            </p>
+          </Col>
+          <Col xs={24} md={24} xl={24}>
+            <p style={{ fontWeight: 600 }}>Message: {modal?.data?.message}</p>
+          </Col>
+        </Row>
+      </Modal>
     </div>
   )
 }
--- src/components/service/authService.js
+++ src/components/service/authService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const login = async (params) => {
   const res = await axios({
     method: 'post',
-    url: 'http://127.0.0.1:8000/api/login',
+    url: process.env.REACT_APP_API_ENDPOINT + '/login',
     data: params,
   }).then((response) => response)
   return res.data
