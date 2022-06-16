2eccfc994302d362d5e98290ffd7f578b71da68a
Do Thi Khanh Linh 2 <linhdtk2@vnext.vn>
2022-06-16 13:35:26 +0700

    delete unused file

--- src/components/layout/header/listItem/Index.js
+++ src/components/layout/header/listItem/Index.js
@@ -11,31 +11,30 @@ const Index = () => {
   return (
     <div className={styles.ListItem}>
       <Row style={{ width: '100%', height: '100%' }}>
-        {role === 'Manager' ||
-          ('Admin' && (
-            <>
-              <Col xs={24} md={1} xl={3}></Col>
-              <Col xs={24} md={7} xl={6} className={styles.column}>
-                <Link to="/" className={styles.formGroup}>
-                  <i className="fa-solid fa-house-chimney"></i>
-                  <h4 style={{ color: 'white' }}>HOME</h4>
-                </Link>
-              </Col>
-              <Col xs={24} md={7} xl={6} className={styles.column}>
-                <Link to="/timesheet" className={styles.formGroup}>
-                  <i className="fa-solid fa-business-time"></i>
-                  <h4 style={{ color: 'white' }}>TIMESHEET</h4>
-                </Link>
-              </Col>
+        {(role === 'Admin' || 'Manager') && (
+          <>
+            <Col xs={24} md={1} xl={3}></Col>
+            <Col xs={24} md={7} xl={6} className={styles.column}>
+              <Link to="/" className={styles.formGroup}>
+                <i className="fa-solid fa-house-chimney"></i>
+                <h4 style={{ color: 'white' }}>HOME</h4>
+              </Link>
+            </Col>
+            <Col xs={24} md={7} xl={6} className={styles.column}>
+              <Link to="/timesheet" className={styles.formGroup}>
+                <i className="fa-solid fa-business-time"></i>
+                <h4 style={{ color: 'white' }}>TIMESHEET</h4>
+              </Link>
+            </Col>
 
-              <Col xs={24} md={7} xl={6} className={styles.column}>
-                <Link to={data || '/'} className={styles.formGroup}>
-                  <i className="fa-solid fa-bars-progress"></i>
-                  <h4 style={{ color: 'white' }}>MANAGER</h4>
-                </Link>
-              </Col>
-            </>
-          ))}
+            <Col xs={24} md={7} xl={6} className={styles.column}>
+              <Link to={data || '/'} className={styles.formGroup}>
+                <i className="fa-solid fa-bars-progress"></i>
+                <h4 style={{ color: 'white' }}>MANAGER</h4>
+              </Link>
+            </Col>
+          </>
+        )}
         {role === 'Member' && (
           <>
             <Col xs={24} md={8} xl={10}></Col>
